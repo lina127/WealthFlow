89d6b7fa48479b0cfb8e07793fc77ef7cc1b74f6
Jin Oh <jin.oh@bngg.onmicrosoft.com>
2021-10-11 19:23:12 -0400

    Commit

---
+++ Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WealthFlow.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult Transactions()
+        {
+            return View();
+        }
+    }
+}
