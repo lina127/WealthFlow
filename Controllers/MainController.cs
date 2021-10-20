2c938755b495a2b06197ea3dfcffcd60a8c3e6bd
Yehyun Kim <kimoliv127@naver.com>
2021-10-20 01:21:55 -0400

    Commit

--- Controllers/MainController.cs
+++ Controllers/MainController.cs
@@ -20,7 +20,7 @@ namespace WealthFlow.Controllers
         {
             if (IsSessionValid(out User? user))
             {
-                List<Card> cards = _dbContext.Card.Where(o => o.UserId == user.UserId).ToList();
+                List<Card> cards = _dbContext.Card.Where(o => o.UserId == user.UserId && o.Status == "Active").ToList();
                 
                 List<Transaction> transactions = _dbContext.Transaction.Where(o => o.Card.UserId == user.UserId).ToList();
 
@@ -103,13 +103,20 @@ namespace WealthFlow.Controllers
                             continue;
                         date = DateTime.Parse(columns[0]);
                         merchant = columns[1].Trim();
-                        if (columns[2] != null && columns[2] != "" && Convert.ToDecimal(columns[2]) > 0)
+                        if (columns[3] != null && columns[3] != "" &&  columns[5] != null && columns[5] != "" && Convert.ToDecimal(columns[3]) > 0)
                         {
-                            amount = Convert.ToDecimal(columns[2]) * -1;
+                            amount = Convert.ToDecimal(columns[3]) * -1;
                         }
                         else
                         {
-                            amount = Convert.ToDecimal(columns[3]);
+                            if (columns[5] != null || columns[5] != "")
+                            {
+                                amount = Convert.ToDecimal(columns[4]);
+                            }
+                            else
+                            {
+                                amount = Convert.ToDecimal(columns[3]);
+                            }
                         }
                     }
                     // Exclude => skip to next item
@@ -189,8 +196,8 @@ namespace WealthFlow.Controllers
             {
                 List<Keyword> keyword = _dbContext.Keyword.Where(o => o.Category.UserId == user.UserId).OrderBy(o => o.Category.Name).ThenBy(o => o.Name).ToList();
                 List<Category> category = _dbContext.Category.Where(o => o.UserId == user.UserId).OrderBy(o => o.Type).ThenBy(o => o.Name).ToList();
-                List<ExcludeKeyword> excludeKeyword = _dbContext.ExcludeKeyword.Where(o => o.Card.UserId == user.UserId).OrderBy(o => o.Name).ToList();
-                List<Card> card = _dbContext.Card.Where(o => o.UserId == user.UserId).ToList();
+                List<ExcludeKeyword> excludeKeyword = _dbContext.ExcludeKeyword.Where(o => o.Card.UserId == user.UserId && o.Card.Status == "Active").OrderBy(o => o.Name).ToList();
+                List<Card> card = _dbContext.Card.Where(o => o.UserId == user.UserId && o.Status == "Active").ToList();
                 DataDTO dataDTO = new DataDTO(keyword, category, card, excludeKeyword);
                 return View(dataDTO);
             }
